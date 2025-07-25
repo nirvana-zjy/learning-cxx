b173f2201759b18d9e91b6b68dc07177c3f2ed43
nirvana_zjy <1643776722@qq.com>
2025-07-25 11:51:02 +0800

    完成C++习题库

--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -13,12 +13,12 @@ class Fibonacci {
     int cached;
 
 public:
-    // TODO: 实现构造器
-    // Fibonacci()
+    // 实现无参构造器，初始化前两个斐波那契数和已缓存项数
+    Fibonacci() : cache{0, 1}, cached(2) {}
 
-    // TODO: 实现正确的缓存优化斐波那契计算
+    // 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
--- exercises/22_class_template/main.cpp
+++ exercises/22_class_template/main.cpp
@@ -10,6 +10,10 @@ struct Tensor4D {
     Tensor4D(unsigned int const shape_[4], T const *data_) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; ++i) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memcpy(data, data_, size * sizeof(T));
     }
@@ -28,6 +32,28 @@ struct Tensor4D {
     // 则 `this` 与 `others` 相加时，3 个形状为 `[1, 2, 1, 4]` 的子张量各自与 `others` 对应项相加。
     Tensor4D &operator+=(Tensor4D const &others) {
         // TODO: 实现单向广播的加法
+        unsigned int this_size = shape[0] * shape[1] * shape[2] * shape[3];
+        
+        for (unsigned int i = 0; i < this_size; ++i) {
+            // 计算当前元素在4维中的索引
+            unsigned int idx0 = i / (shape[1] * shape[2] * shape[3]);
+            unsigned int idx1 = (i % (shape[1] * shape[2] * shape[3])) / (shape[2] * shape[3]);
+            unsigned int idx2 = (i % (shape[2] * shape[3])) / shape[3];
+            unsigned int idx3 = i % shape[3];
+            
+            // 计算在others中对应的索引（考虑广播）
+            unsigned int other_idx0 = (others.shape[0] == 1) ? 0 : idx0;
+            unsigned int other_idx1 = (others.shape[1] == 1) ? 0 : idx1;
+            unsigned int other_idx2 = (others.shape[2] == 1) ? 0 : idx2;
+            unsigned int other_idx3 = (others.shape[3] == 1) ? 0 : idx3;
+            
+            unsigned int other_linear_idx = other_idx0 * (others.shape[1] * others.shape[2] * others.shape[3]) +
+                                          other_idx1 * (others.shape[2] * others.shape[3]) +
+                                          other_idx2 * others.shape[3] +
+                                          other_idx3;
+            
+            data[i] += others.data[other_linear_idx];
+        }
         return *this;
     }
 };
